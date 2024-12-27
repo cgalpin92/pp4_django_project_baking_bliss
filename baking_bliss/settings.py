940c27a089c4e50e532fc4eca9a55c041b42ed81
Charlotte Galpin <charlotte.galpin92@gmail.com>
2024-12-27 23:13:29 +0000

    connect baking_bliss to PostgreSQL database

--- baking_bliss/settings.py
+++ baking_bliss/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&0#)ub*_xguet2=-=pn3257b$8g6+4$5cqmt0d1o9!jx&#v!ir'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-cgalpin92-pp4djangoproj-dkj2cbccfny.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
@@ -75,11 +79,15 @@ WSGI_APPLICATION = 'baking_bliss.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
