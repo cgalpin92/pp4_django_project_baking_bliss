88a40420df63c62d9393f3bd65053d3f57d310a3
Charlotte Galpin <charlotte.galpin92@gmail.com>
2024-12-28 19:13:06 +0000

    reverse migation of IngredientName model to fix non-nullable error when adding to model

--- recipes/models.py
+++ recipes/models.py
@@ -4,3 +4,5 @@ from django.contrib.auth.models import User
 # Create your models here.
 class IngredientName(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name="ingredient_author")
+    approved = models.BooleanField(default=False)
