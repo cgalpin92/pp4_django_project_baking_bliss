cfbf59202594934e955375f0739d551b4a67a1e0
Charlotte Galpin <charlotte.galpin92@gmail.com>
2024-12-28 19:56:59 +0000

    add Ingredient model to database

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import IngredientName
+from .models import IngredientName, Ingredient
 
 # Register your models here.
 admin.site.register(IngredientName)
+admin.site.register(Ingredient)
--- recipes/models.py
+++ recipes/models.py
@@ -6,3 +6,29 @@ class IngredientName(models.Model):
     name = models.CharField(max_length=100, unique=True)
     author = models.ForeignKey(User, on_delete=models.PROTECT, related_name="ingredient_author")
     approved = models.BooleanField(default=False)
+
+
+class Ingredient(models.Model):
+    TEASPOON = "tsp"
+    TABLESPOON = "tbsp"
+    FLUID_OUNCE = "fl oz"
+    CUP = "cup"
+    GRAM = "g"
+    KILOGRAM = "kg"
+    LITER = "l"
+    MILLILITER = "ml"
+    CENTIMETER = "cm"
+    UNIT_CHOICES = [
+        (TEASPOON, "teaspoon"),
+        (TABLESPOON, "tablespoon"),
+        (FLUID_OUNCE, "fluid ounce"),
+        (CUP, "cup"),
+        (GRAM, "gram"),
+        (KILOGRAM, "kilogram"),
+        (LITER, "liter"),
+        (MILLILITER, "milliter"),
+        (CENTIMETER, "centimeter"),
+    ]
+    ingredient_name = models.ForeignKey(IngredientName, on_delete=models.CASCADE, related_name="ingredient_name")
+    measurement = models.IntegerField(default=0)
+    unit = models.CharField(max_length=20, choices=UNIT_CHOICES, default=GRAM)
