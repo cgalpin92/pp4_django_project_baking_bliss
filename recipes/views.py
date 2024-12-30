32a65c32939cf9ae04bf616a5b3ed78f24ffa6ce
Charlotte Galpin <charlotte.galpin92@gmail.com>
2024-12-30 15:51:49 +0000

    creates initial category_list.html with initial boilerplate code and creates initial class based view within recipes/views.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.views import generic
+from .models import IngredientName, Ingredient, Category, Recipe
 
 # Create your views here.
-def recipe(request):
-    return HttpResponse("The recipe page works!")
+# def recipe(request):
+#     return HttpResponse("The recipe page works!")
+
+class CategoryList(generic.ListView):
+    model = Category
