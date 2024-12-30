623fa64f916463eea5f7e81eb3eaebe7ec6896c9
Charlotte Galpin <charlotte.galpin92@gmail.com>
2024-12-30 17:23:13 +0000

    adds RecipeAdmin class to recipe/admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import IngredientName, Ingredient, Category, Recipe
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+    list_display = ('recipe_name', 'category', 'difficulty')
+    search_fields = ['category']
+    list_filter = ('difficulty',)
+    prepopulated_fields = {'slug': ('recipe_name',)}
 
 # Register your models here.
 admin.site.register(IngredientName)
 admin.site.register(Ingredient)
 admin.site.register(Category)
-admin.site.register(Recipe)
