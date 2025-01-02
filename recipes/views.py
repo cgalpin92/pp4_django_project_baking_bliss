a7d119f76a09d1bfe778319256ffe2c622232da1
Charlotte Galpin <charlotte.galpin92@gmail.com>
2025-01-02 20:15:47 +0000

    remove recipe_list.html, remove RecipeList view and url pattern for RecipeView

--- recipes/views.py
+++ recipes/views.py
@@ -10,3 +10,8 @@ from .models import IngredientName, Ingredient, Category, Recipe
 class CategoryList(generic.ListView):
     queryset = Category.objects.all()
     template_name = "recipes/index.html"
+
+
+class RecipeList(generic.ListView):
+    queryset = Recipe.objects.all()
+    template_name = "recipes/recipe_list.html"
