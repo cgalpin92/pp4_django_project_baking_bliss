6b31503b52413a754999888d44a7a4f6fe8f6657
Charlotte Galpin <charlotte.galpin92@gmail.com>
2025-01-04 15:55:52 +0000

    create category.html and recipe_category.html, create classed based view CategoryList, adds content to category.html and links to index.html

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
    # path('', views.CategoryList.as_view(),
     #name='home'),
     path('', views.RecipeList.as_view(), name='home'),
+    path('categories/', views.CategoryList.as_view(), name='category'),
     path('<slug:slug>/', views.recipe_detail, name="recipe_detail"),
+    
+    path('category/<category>', views.category_recipes, name="recipe_category")
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,19 +1,40 @@
 from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse
 from django.views import generic
-from .models import IngredientName, Ingredient, Category, Recipe, Comment
+from .models import IngredientName, Ingredient, Category, Recipe, Comment, User
 
 # Create your views here.
 # def recipe(request):
 #     return HttpResponse("The recipe page works!")
 
-#class CategoryList(generic.ListView):
-    #queryset = Category.objects.all()
-    #template_name = "recipes/index.html"
+
 
 class RecipeList(generic.ListView):
     queryset = Recipe.objects.all()
     template_name = "recipes/index.html"
+
+
+class CategoryList(generic.ListView):
+    queryset = Category.objects.all()
+    template_name = "recipes/category.html"
+
+
+
+
+def category_recipes(request, category):
+    
+    queryset = Recipe.objects.filter(
+        category__name__contains = category
+    )
+    return render(
+        request,
+        "recipes/recipe_category.html",
+        {
+            "category": category,
+            "recipe": recipe,
+        }
+    )
+
 
 
 def recipe_detail(request, slug):
@@ -21,9 +42,8 @@ def recipe_detail(request, slug):
     queryset = Recipe.objects.filter(status=1)
     recipe = get_object_or_404(queryset, slug=slug)
     ingredients = Ingredient.objects.all()
-
-    
     comments = recipe.comment.all()
+    
 
     return render(
         request,
