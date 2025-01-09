49b7c361fad23f3cb48881aa3f3522ae8e0dbf60
Charlotte Galpin <charlotte.galpin92@gmail.com>
2025-01-09 23:05:22 +0000

    remove fixtures folder and commented out urls as no longer needed

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,11 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-   # path('', views.CategoryList.as_view(),
-    #name='home'),
- #   path('', views.RecipeList.as_view(), name='home'),
- #  path('', views.recipe_list, name='home'),
- #   path('categories/', views.CategoryList.as_view(), name='category'),
    path('', views.CategoryList.as_view(), name="home"),    
    path('all_recipes/', views.RecipeList.as_view(), name='recipes'),
    path('recipe_upload/', views.recipe_upload, name="recipe_upload"),
