30d4ae1056d7fef1e0665868e899b1e6ef54c749
Charlotte Galpin <charlotte.galpin92@gmail.com>
2025-01-13 11:49:49 +0000

    set debug to false and run collect static files for deployment

--- staticfiles/js/recipe.js
+++ staticfiles/js/recipe.js
@@ -3,10 +3,19 @@ bootstrap.Modal(document.getElementById("deleteRecipeModal"));
 const deleteButtons = document.getElementsByClassName("btn-delete-recipe");
 const deleteConfirm = document.getElementById("deleteConfirm");
 
-
+/**
+ * gives functionality to the delete button in my_recipe.html
+ * 
+ * For each deleteButton an event listener is added to retrieve the 
+ * recipe's ID
+ * This then Updates the `recipe_delete` link's href to point to the 
+ * deletion endpoint for the specific recipe.
+ * This then triggers the modal to check that the user want to delete
+ * the recipe before doing so
+ */
 for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
-        let recipeId = e.target.getAttribute("recipe_id");
+        let recipeId = e.target.getAttribute("data-recipe_id");
         deleteRecipeConfirm.href = `recipe_delete/${recipeId}`;
         deleteModal.show();
     });
