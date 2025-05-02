f5c42257302831cccce14021547342ecae97d6f2
cgalpin92 <167214716+cgalpin92@users.noreply.github.com>
2025-05-02 19:09:01 +0100

    cloned repository to vscode, created virtual environment and new env.py file and installed dependencies. fixed error of exposed secret key, new secret key generated and added to env.py

--- baking_bliss/settings.py
+++ baking_bliss/settings.py
@@ -24,13 +24,14 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&0#)ub*_xguet2=-=pn3257b$8g6+4$5cqmt0d1o9!jx&#v!ir'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-cgalpin92-pp4djangoproj-dkj2cbccfny.ws.codeinstitute-ide.net',
-                '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1',
+                 '.herokuapp.com']
 
 
 # Application definition
@@ -108,7 +109,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.codeinstitute-ide.net/",
+    "https://localhost",
     "https://*herokuapp.com/"]
 
 # Password validation
